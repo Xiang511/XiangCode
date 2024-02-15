06f3faa16b50d871cb515c11ca7316193ef1a6e4
詳 <120042360+Xiang511@users.noreply.github.com>
2024-02-16 02:47:45 +0800

    新增 進度條
    - code review

--- 皇室戰爭API/個人成就.py
+++ 皇室戰爭API/個人成就.py
@@ -3,13 +3,16 @@ import requests
 import json
 import re
 import time
+from tqdm import tqdm
 start_time = time.time()
+# enter your key
 API_KEY = ""
 headers = {
     "Authorization": "Bearer {}".format(API_KEY)
 }
 
-player_tags = []
+# enter your tag
+player_tags = [ ]
               
 wb = openpyxl.Workbook()
 ws = wb.active
@@ -23,40 +26,12 @@ ws["F1"] = "戰旗收集次數"
 ws["G1"] = "星光點數"
 ws["H1"] = "全部經驗值"
 
-def clean_result(result):
-    if result is None:
-        return ""
-    return result[result.rfind(" ") + 1:].split("}")[0]
 
-
-def keep_numbers(string):
-  
-  filtered_string = filter(str.isdigit, string)
-  return "".join(filtered_string)
-
-def get_numbers_by_position_and_delete(variable, start_position, end_position):
-
-  numbers = str(variable)[start_position:end_position]
-  filtered_numbers = filter(str.isdigit, numbers)
-  return "".join(filtered_numbers)
-
-x=0
-
-
-
-
-for player_tag in player_tags:
+for player_tag in tqdm(player_tags):
     response = requests.get(f"https://api.clashroyale.com/v1/players/{player_tag}", headers=headers)
     
     player_data = response.json()
-    best = json.dumps(player_data.get("bestPathOfLegendSeasonResult"))
-    last = json.dumps(player_data.get("lastPathOfLegendSeasonResult"))
 
-    best2 = clean_result(best)
-    last2= clean_result(last)
-
-    best3 = get_numbers_by_position_and_delete(keep_numbers(best), 2, 6)
-    last3 = get_numbers_by_position_and_delete(keep_numbers(last), 2, 6)
 
     y=player_data["tag"]
 
@@ -94,13 +69,9 @@ for player_tag in player_tags:
 
 
 
-
-
     ws.append([
     player_data["name"],Classic12WinsCout,Grand12WinsCout,YearsPlayedCout,EmoteCollectionCout,BannerCollectionCout,starPoints,totalExpPoints])
 
-    x+=1
-    print(x)
 wb.save("TCRS.xlsx")
 
 end_time = time.time()
