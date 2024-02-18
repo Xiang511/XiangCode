6d0d4e1307b3727b5401162e7e48ed4083336e88
LiXiang <toby91511@gmail.com>
2024-02-18 20:53:23 +0800

    Code Review Player.py

--- 皇室戰爭API/Player.py
+++ 皇室戰爭API/Player.py
@@ -1,17 +1,26 @@
 import openpyxl
 import requests
 import json
-import re
 import time
 from tqdm import tqdm
+
+#計算起始時間
 start_time = time.time()
+
+# 設定 API 金鑰
+# 前往 https://developer.clashroyale.com/#/ 取得
 API_KEY = ""
-# 填入api key
+
 headers = {
     "Authorization": "Bearer {}".format(API_KEY)
 }
 
+# enter your tag
+# ex: '%2322R920J00','%2312345678'
+
 player_tags = ["%2322R920J00", "%23V99082R9C"]
+
+
 
 wb = openpyxl.Workbook()
 ws = wb.active
@@ -25,11 +34,19 @@ def clean_result(result):
     if result is None:
         return ""
     return result[result.rfind(" ") + 1:].split("}")[0]
+#ex:
+#   best = json.dumps(player_data.get("bestPathOfLegendSeasonResult"))
+#   回傳值為{"leagueNumber": 10, "trophies": 2416, "rank": 2573}
+#
+#   另 best2 = clean_result(best)
+#   則 best2 = 2573
 
 for player_tag in tqdm(player_tags):
+
     response = requests.get(f"https://api.clashroyale.com/v1/players/{player_tag}", headers=headers)
     
     player_data = response.json()
+
     best = json.dumps(player_data.get("bestPathOfLegendSeasonResult"))
     last = json.dumps(player_data.get("lastPathOfLegendSeasonResult"))
 
@@ -43,5 +60,6 @@ for player_tag in tqdm(player_tags):
 
 wb.save("Player.xlsx")
 
+#計算結束時間
 end_time = time.time()
 print(f"執行時間：{end_time - start_time}")
